47490554c939ee1ba9551483293c9bde758d4043
yenz0redd <375293774737@yandex.ru>
2019-03-04 19:45:52 +0300

    1time connection by sickets

---
+++ ClientTcp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string ip = "127.0.0.1"; 
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Connect(tcpEndPoint);
+
+                Console.Write("message to send : ");
+
+                var message = Console.ReadLine();
+
+                if (message.ToUpper() == "EXIT")
+                {
+                    Console.WriteLine("client stopped..");
+                    return;
+                }
+
+                var data = Encoding.UTF8.GetBytes(message);
+
+                tcpSocket.Send(data);
+
+                var buffer = new byte[256];
+                var size = 0;
+                var answer = new StringBuilder();
+
+                do
+                {
+                    size = tcpSocket.Receive(buffer);
+                    answer.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                } while (tcpSocket.Available > 0);
+
+                Console.WriteLine(answer.ToString());
+
+                if (!tcpSocket.Connected)
+                {
+                    tcpSocket.Shutdown(SocketShutdown.Both);
+                    tcpSocket.Close();
+                    tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                }
+           
+        }
+    }
+}
---
+++ ServerTcp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTcp
+{
+    class MainClass
+    {
+        public static void Listening(Socket listener)
+        {
+            var buffer = new byte[256];
+            var size = 0;
+            var data = new StringBuilder();
+
+            do
+            {
+                Console.WriteLine("server listen..");
+                size = listener.Receive(buffer);
+                data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                Console.WriteLine(data);
+                var feedBack = "DONE!";
+                listener.Send(Encoding.UTF8.GetBytes(feedBack));
+                buffer = null;
+
+            } while (size != 0);
+
+            listener.Shutdown(SocketShutdown.Both);
+            listener.Close();
+        }
+
+        public static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(5);
+
+            while (true)
+            {
+                var listener = tcpSocket.Accept();
+                Task.Run(() => Listening(listener));
+            }
+        }
+    }
+}
